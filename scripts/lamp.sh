9f4d9bc9235fdb41e7e2449b6955239fe66dbcd2
Hiroo Tolani <hiroo@blimpdigital.com>
2017-05-31 19:27:48 +0700

    shortened script

--- scripts/lamp.sh
+++ scripts/lamp.sh
@@ -31,66 +31,10 @@ echo "...Done!"
 echo " "
 
 echo "Setting up Apache..."
-sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak 
-sudo sed -i 's_/var/www/html_/var/www/_' /etc/httpd/conf/httpd.conf
+sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
 sudo sed -i 's_#ServerName www.example.com:80_ServerName www.abox.dev:80_' /etc/httpd/conf/httpd.conf
 sudo sed -i 's_User apache_User vagrant_' /etc/httpd/conf/httpd.conf
 sudo sed -i 's_Group apache_Group vagrant_' /etc/httpd/conf/httpd.conf
-sudo sh -c 'echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf'
-sudo mkdir /etc/httpd/sites-available/
-sudo mkdir /etc/httpd/sites-enabled/
-sudo rm -fr /var/www/html
-
-sudo sed -i 's_ScriptAlias /cgi-bin/_#ScriptAlias /cgi-bin/_' /etc/httpd/conf/httpd.conf
-sudo sed -i "s/AllowOverride None/AllowOverride All/g" /etc/httpd/conf/httpd.conf
-
-echo "--> Configuring aBox.dev Virtualhost"
-sudo mkdir /var/www/abox.dev
-sudo mkdir /var/www/abox.dev/html
-sudo touch /var/www/abox.dev/html/index.html
-sudo sh -c 'echo "This is abox.dev" >> /var/www/abox.dev/html/index.html'
-sudo touch /etc/httpd/sites-available/abox.dev.conf
-sudo sh -c 'echo "<VirtualHost *:80>
-        ServerName      www.abox.dev
-        ServerAlias     abox.dev
-        DocumentRoot    /var/www/abox.dev/html
-        ErrorLog        /var/www/abox.dev/error.log
-        CustomLog       /var/www/abox.dev/requests.log  combined
-</VirtualHost>" >> /etc/httpd/sites-available/abox.dev.conf'
-sudo ln -s /etc/httpd/sites-available/abox.dev.conf /etc/httpd/sites-enabled/abox.dev.conf 
-echo ".. Done!"
-
-echo "--> Configuring example1.dev Virtualhost"
-sudo mkdir /var/www/example1.dev
-sudo mkdir /var/www/example1.dev/html
-sudo touch /var/www/example1.dev/html/index.html
-sudo sh -c 'echo "This is example1.dev" >> /var/www/example1.dev/html/index.html'
-sudo touch /etc/httpd/sites-available/example1.dev.conf
-sudo sh -c 'echo "<VirtualHost *:80>
-        ServerName      www.example1.dev
-        ServerAlias     example1.dev
-        DocumentRoot    /var/www/example1.dev/html
-        ErrorLog        /var/www/example1.dev/error.log
-        CustomLog       /var/www/example1.dev/requests.log  combined
-</VirtualHost>" >> /etc/httpd/sites-available/example1.dev.conf'
-sudo ln -s /etc/httpd/sites-available/example1.dev.conf /etc/httpd/sites-enabled/example1.dev.conf 
-echo "--> .. Done!"
-
-echo "--> Configuring example2.dev Virtualhost"
-sudo mkdir /var/www/example2.dev
-sudo mkdir /var/www/example2.dev/html
-sudo touch /var/www/example2.dev/html/index.html
-sudo sh -c 'echo "This is example2.dev" >> /var/www/example2.dev/html/index.html'
-sudo touch /etc/httpd/sites-available/example2.dev.conf
-sudo sh -c 'echo "<VirtualHost *:80>
-        ServerName      www.example2.dev
-        ServerAlias     example2.dev
-        DocumentRoot    /var/www/example2.dev/html
-        ErrorLog        /var/www/example2.dev/error.log
-        CustomLog       /var/www/example2.dev/requests.log  combined
-</VirtualHost>" >> /etc/httpd/sites-available/example2.dev.conf'
-sudo ln -s /etc/httpd/sites-available/example2.dev.conf /etc/httpd/sites-enabled/example2.dev.conf 
-echo "--> .. Done!"
 
 sudo systemctl enable httpd.service > /dev/null 2>&1
 sudo systemctl restart httpd.service > /dev/null 2>&1
@@ -116,9 +60,7 @@ echo "MySQL Password: vagrant"
 echo " "
 echo "You can copy and paste the following to your hosts file: "
 echo " " 
-echo "192.168.33.10	abox.dev		www.abox.dev"
-echo "192.168.33.10	example1.dev	www.example1.dev"
-echo "192.168.33.10	example2.dev	www.example2.dev"
+echo "192.168.33.10	abox.dev    www.abox.dev"
 echo " "
 echo "The 192.168.33.10 is the IP Address in your Vagrantfile."
 echo "If you have opted to choose a different IP Address, please adjust accordingly."
